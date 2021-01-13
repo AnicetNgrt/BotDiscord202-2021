a3ecdb67625cbabd1090d2a4a539664f54c8dd5d
Anicet Ngrt <anicet.nougaret@zohomail.eu>
2021-01-13 18:23:28 +0100

    added help and source commands

--- index.js
+++ index.js
@@ -18,6 +18,17 @@ client.on('message', message => {
     if (command === 'ping') {
         message.reply('Pong!');
     }
+    else if (['source','code'].indexOf(command) !== -1) {
+        message.reply('https://github.com/AnicetNgrt/BotDiscord202-2021');
+    }
+    else if (['?','help','aide'].indexOf(command) !== -1) {
+        let ans = '';
+        ans += 'Aide TIBO-MATIC:\n';
+        ans += 'ping -> pong\n';
+        ans += 'source -> lien source\n';
+        ans += 'help -> afficher l\'aide\n'
+        message.reply(ans);
+    }
 });
 
 client.login(process.env.DISCORD_TOKEN);
